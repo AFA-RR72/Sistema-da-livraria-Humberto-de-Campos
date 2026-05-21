dd052226bd15a087ac4552ec9de205a1c8f8d9be
Andrei Freire de Almeida <andreifalmeida08@gmail.com>
2026-05-20 23:40:05 -0400

    corrigindo conexão para ser usado no localhost e no infinityfree

--- MVC/controller/funcoes.php
+++ MVC/controller/funcoes.php
@@ -4,7 +4,21 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 }
 function conexao(){
-$conn = mysqli_connect('localhost', 'root', '', 'livraria');
+$localhost = $_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER=['HTTP_HOST'] == '127.0.0.1';
+
+if($localhost){
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $database = 'livraria';
+}else{
+    $host = 'sql100.infinityfree.com';
+    $user = 'if0_41492011';
+    $password = 'afa011429';
+    $database = 'if0_41492011_livraria';
+}
+
+$conn = mysqli_connect($host, $user, $password, $database);
 
 mysqli_set_charset($conn, 'utf8');
 
